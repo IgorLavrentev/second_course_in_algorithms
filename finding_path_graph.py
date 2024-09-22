d50e495f3454f19fdb9379ebdb4779cc811977c7
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-09-22 23:16:23 +0300

    finding_path_graph.py correction 2

--- finding_path_graph.py
+++ finding_path_graph.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 class Stack:
     def __init__(self):
         self.stack = []
@@ -23,26 +25,26 @@ class Stack:
 
 class Vertex:
 
-    def __init__(self, val):
-        self.Value = val
-        self.Hit = False
+    def __init__(self, val: int):
+        self.Value: int = val
+        self.Hit: bool = False
 
 
 class SimpleGraph:
 
-    def __init__(self, size):
-        self.max_vertex = size
-        self.m_adjacency = [[0] * size for _ in range(size)]
-        self.vertex = [None] * size
+    def __init__(self, size: int):
+        self.max_vertex: int = size
+        self.m_adjacency: list = [[0] * size for _ in range(size)]
+        self.vertex: list = [None] * size
 
-    def AddVertex(self, v):
+    def AddVertex(self, v: int) -> None:
         # добавление новой вершины
         for i in range(len(self.vertex)):
             if self.vertex[i] is None:
                 self.vertex[i] = Vertex(v)
                 break
 
-    def RemoveVertex(self, v):
+    def RemoveVertex(self, v: int) -> None:
         # код удаления вершины со всеми её рёбрами
         if self.vertex[v] is not None:
             self.vertex[v] = None
@@ -50,23 +52,23 @@ class SimpleGraph:
         for i in range(self.max_vertex):
             self.m_adjacency[i][v] = 0
 
-    def IsEdge(self, v1, v2):
+    def IsEdge(self, v1: int, v2: int) -> bool:
         # True если есть ребро между вершинами v1 и v2
         if self.m_adjacency[v2][v1] == 1:
             return True
         return False
 
-    def AddEdge(self, v1, v2):
+    def AddEdge(self, v1: int, v2: int) -> None:
         # добавление ребра между вершинами v1 и v2
         if self.m_adjacency[v2][v1] != 1:
             self.m_adjacency[v2][v1] = 1
 
-    def RemoveEdge(self, v1, v2):
+    def RemoveEdge(self, v1: int, v2: int) -> None:
         # удаление ребра между вершинами v1 и v2
         if self.m_adjacency[v2][v1] == 1:
             self.m_adjacency[v2][v1] = 0
 
-    def _DepthFirstSearch(self, stack_for_graph, vertwx_x, VFrom, VTo):
+    def _DepthFirstSearch(self, stack_for_graph: Stack, vertwx_x: Vertex, VFrom: int, VTo: int) -> Union[Stack, bool]:
 
         for n in range(self.max_vertex):
             # если узел найден, записываем его в стек и возвращаем сам стек как результат работы
@@ -94,16 +96,16 @@ class SimpleGraph:
             stack_for_graph, vertwx_x, self.vertex.index(vertwx_x), VTo
         )
 
-    def DepthFirstSearch(self, VFrom, VTo):
+    def DepthFirstSearch(self, VFrom: int, VTo: int) -> Union[list, bool]:
 
         # очищаем все дополнительные структуры данных: делаем стек пустым,
         # а все вершины графа отмечаем как непосещённые
-        stack_for_graph = Stack()
+        stack_for_graph: Stack = Stack()
         for _ in range(stack_for_graph.size()):
             stack_for_graph.pop()
 
         # выбираем текущую вершину
-        vertwx_x = self.vertex[0]
+        vertwx_x: Vertex = self.vertex[VFrom]
 
         # фиксируем вершину как посещённую
         vertwx_x.Hit = True
@@ -111,12 +113,12 @@ class SimpleGraph:
         # помещаем вершину в стек
         stack_for_graph.push(vertwx_x)
 
-        result_recursive_function = self._DepthFirstSearch(
+        result_recursive_function: object = self._DepthFirstSearch(
             stack_for_graph, vertwx_x, VFrom, VTo
         )
         if result_recursive_function is False:
             return False
-        list_nodes_Vertex = []
+        list_nodes_Vertex: list = []
         for _ in range(result_recursive_function.size()):
             list_nodes_Vertex.append(result_recursive_function.pop())
         return list_nodes_Vertex
